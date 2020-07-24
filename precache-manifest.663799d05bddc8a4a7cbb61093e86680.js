61ed0eb16051584f82d6a93c1b017554d662ab65
Joshua-Salvador <424joshuae.b.s@gmail.com>
2020-07-24 10:02:25 +0800

    Updates

---
+++ precache-manifest.663799d05bddc8a4a7cbb61093e86680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10567ad4a4a0e07c0a15ab7b569f6eaf",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "c79648ccba86fd4eec1d",
+    "url": "/todo-react/static/css/main.382f6951.chunk.css"
+  },
+  {
+    "revision": "06475a2e631d7395b0c8",
+    "url": "/todo-react/static/js/2.1c18fa49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.1c18fa49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79648ccba86fd4eec1d",
+    "url": "/todo-react/static/js/main.4d6214a5.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
